cef175a0d78767152921a158d1c0b6d6a24542c5
Kate Malinis <kgmalinis99@gmail.com>
2021-03-08 23:02:06 -0600

    adding

---
+++ main.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '!';
+
+client.once('ready', () => 
+{
+    console.log('we live on the web');
+});
+
+client.on('message', message => 
+{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    const command = args.shift().toLowerCase();
+
+    if(command === 'howdy')
+    {
+        message.channel.send('howdy partner');
+    }
+    else if (command == 'check')
+    {
+        message.channel.send('drink ya water fix ya posture take ya vitamins');
+    }
+    else if (command == 'bitsplease')
+    {
+        message.channel.send('every saturday @ 7 cst 8) https://www.twitch.tv/gnarlolita_');
+    }
+});
+
+client.login('ODE4NzAyNzA4Mzc3NzgwMjc0.YEb6UA.YtKm4zqlaZnIIN2MOk-6zyh3eCk');
